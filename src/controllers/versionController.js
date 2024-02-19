18c2560ac44572332765f8a349f555f836731e2e
pavandwivedi <pavandwivedi35@gmail.com>
2024-02-19 14:19:07 +0530

    bubble shooter

--- src/controllers/versionController.js
+++ src/controllers/versionController.js
@@ -2,11 +2,11 @@ import versionModel from "../models/Version.js";
 import {error,success} from "../utills/responseWrapper.utill.js";
 
 export async function insertVersionController(req, res) {
-    console.log("pavan");
+    
     const { version } = req.body;
     try {
-        let existingVersion = await versionModel.findOne({ version });
-        if (existingVersion) {
+        let existingVersion = await versionModel.find({  });
+        if (existingVersion.length === 1) {
             // If the version already exists, update it instead of creating a new one
             existingVersion = await versionModel.findOneAndUpdate({ version }, { version }, { new: true });
             console.log(existingVersion);
@@ -15,7 +15,7 @@ export async function insertVersionController(req, res) {
 
         // If the version doesn't exist, create a new one
         const newVersion = await versionModel.create({ version });
-        console.log(newVersion);
+       
         return res.send(success(200, "New version created"));
     } catch (err) {
         return res.send(error(500, err.message));
