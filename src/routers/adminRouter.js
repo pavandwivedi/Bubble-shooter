185b3a3d4df6e660583c1ea12c8ed4a55c3b23f0
Kuldeep <kuldeepgwstele@gmail.com>
2024-04-15 15:04:12 +0530

    Updated

--- src/routers/adminRouter.js
+++ src/routers/adminRouter.js
@@ -1,9 +1,5 @@
 import express from "express";
-<<<<<<< HEAD
 import { getAllUsers, getKycListController, loginController, signupController, updateKycStatusController ,createChallengeController,updateChallengeController,deleteChallengeController} from "../controllers/adminController.js";
-=======
-import { getAllUsers, getKycListController, loginController, signupController, updateKycStatusController } from "../controllers/adminController.js";
->>>>>>> 7c232746f2f3105338217cbb0175d2312aed975f
 import { checkAdminLogin } from "../middlewares/middlewares.js";
 
 const adminRouter = express.Router();
@@ -14,12 +10,8 @@ adminRouter.post("/login",loginController);
 adminRouter.get("/getallusers",checkAdminLogin,getAllUsers);
 adminRouter.put('/updatekycstatus/:_id',checkAdminLogin,updateKycStatusController);
 adminRouter.get('/getkyclist',checkAdminLogin,getKycListController);
-<<<<<<< HEAD
 adminRouter.post('/createChallenge',checkAdminLogin,createChallengeController);
 adminRouter.put('/updateChallenge/:id',checkAdminLogin,updateChallengeController);
 adminRouter.delete('/deleteChallenge/:id',checkAdminLogin,deleteChallengeController);
-=======
-
->>>>>>> 7c232746f2f3105338217cbb0175d2312aed975f
 
 export default adminRouter;
